e01b0c6090d30c916bf674b9594d6030da82cb0c
vandeson2 <vandeson2@gmail.com>
2025-12-08 09:18:44 +0100

    Inicialización de mi proyecto con React, Vite, Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+  return (
+    <div className='bg-red-500'>
+      <h1 className='text-3xl font-bold underline'>
+        Prueba Tailwind CSS con Vite y React
+      </h1>
+    </div>
+  )
+}
+
+export default App
