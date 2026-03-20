12f2e8ea26522c2a2ec8be5538266e60595f3e6c
vandeson2 <vandeson2@gmail.com>
2026-03-20 18:12:28 +0100

    Actualizacion de la sesión de habilidades

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,28 +5,46 @@ import css from "../assets/icons/css.png";
 import tailwind from "../assets/icons/tailwind.png";
 import git from "../assets/icons/git.png";
 import github from "../assets/icons/github.png";
-import ts from "../assets/icons/ts.png";
+import python from "../assets/icons/python.png";
+import pandas from "../assets/icons/pandas.png";
 import mysql from "../assets/icons/mysql.png";
 import java from "../assets/icons/java.png";
 import firebase from "../assets/icons/firebase.png";
 import go from "../assets/icons/go.png";
 import { motion } from "motion/react";
+import { div } from "motion/react-client";
 
 export function Skills () {
 
-    const skills = [
-        { icon: react, name: 'React.js'},
-        { icon: tailwind, name: 'Tailwind CSS'},
-        { icon: javascript, name: 'JavaScript'},
-        { icon: html, name: 'HTML5'},
-        { icon: css, name: 'CSS3'},
-        { icon: git, name: 'Git'},
-        { icon: github, name: 'GitHub'},
-        { icon: ts, name: 'TypeScript'},
-        { icon: mysql, name: 'MySQL'},
-        { icon: java, name: 'Java'},
-        { icon: firebase, name: 'Firebase'},
-        { icon: go, name: 'Go'},
+    const skillSessions = [
+        {
+            title:'Data Science & Analysis',
+            items: [
+                { icon: python, name: 'Python'},
+                { icon: pandas, name: 'Pandas'},
+                { icon: mysql, name: 'MySQL'},
+            ]
+        },
+        {
+            title: 'Full Stack Develoment',
+            items: [
+                { icon: react, name: 'React.js'},
+                { icon: javascript, name: 'JavaScript'},
+                { icon: tailwind, name: 'Tailwind CSS'},
+                { icon: html, name: 'HTML5'},
+                { icon: css, name: 'CSS3'},
+            ]
+        },
+        {
+            title: 'Backend & Ecosystem',
+            items: [
+                { icon: go, name: 'Go'},
+                { icon: java, name: 'Java'},
+                { icon: firebase, name: 'Firebase'},
+                { icon: git, name: 'Git'},
+                { icon: github, name: 'GitHub'},
+            ]
+        }   
     ]
     return (
         <section
@@ -48,31 +66,34 @@ export function Skills () {
                     <div className="w-70 h-1 bg-gradient-to-r from-cyan-400 to-blue-500 mx-auto"></div>
                 </motion.div>
 
-                <div  className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
-                    {skills.map((skill, index) =>(
-                        <motion.div
-                            initial={{opacity:0, scale:0.8}}
-                            whileInView={{opacity:1, scale:1}}
-                            viewport={{once: true}}
-                            transition={{duration: 0.5, delay: index * 0.5}}
-                            whileHover={{scale: 1.1, y:-10}}
-                            key={skills.name}
-                            className="flex flex-col justify-center items-center gap-4 bg-white/5 group p-5 rounded-2xl shadow-2xl backdrop-blur-xl border
-                             border-white/10 hover:border-cyan-400/50 transition-all duration-500 hover:bg-white/10 hover:shadow-cyan-400/20 "
-                        >
-                            <div className="p-4 bg-gradient-to-r from-cyan-500 to-blue-500 group-hover:scale-110 transition-transform duration-300 rounded-xl ">
-                                <img src={skill.icon} alt={skill.name}  className="text-white"/>
-                            </div>
-                            <h3 className="text-white text-center text-sm">{skill.name}</h3>
-                        
-
-
-                        </motion.div>
-
-                    ))}
-                </div>
+                {skillSessions.map((session, sIdex) => (
+                    <div key={session.title} className="mb-16">
+                        <h3 className="text-cyan-400 text-xl font-semibold mb-8 uppercase tracking-widest border-l-4 border-cyan-500 pl-4">
+                            {session.title}
+                        </h3>
+                    <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-5">
+                            {session.items.map((skill, index) => (
+                               <motion.div
+                                    key={skill.name}
+                                    initial={{ opacity: 0, y: 20 }}
+                                    whileInView={{ opacity: 1, y: 0 }}
+                                    viewport={{ once: true }}
+                                    transition={{ delay: index * 0.1 }}
+                                    whileHover={{ y: -5 }}
+                                className="flex flex-col items-center gap-3 bg-white/5 p-3 rounded-2xl border border-white/10 hover:border-cyan-500/50 transition-all group"
+                                >
+                                    <div className= "p-2.5 bg-gradient-to-r from-cyan-500 to-blue-500 group-hover:scale-110 transition-transform duration-300 rounded-xl">
+                                        <img src={skill.icon} alt={skill.name} className="text-white w-6 h-6 md:w-8 md:h-8" />
+                                    </div>
+                                    <span className="text-gray-300 text-sm font-medium group-hover:text-white transition-colors">
+                                        {skill.name}
+                                    </span>
+                                </motion.div> 
+                            ))}
+                        </div>
+                    </div>
+                ))}
             </div>
-
         </section>
     )
 }
